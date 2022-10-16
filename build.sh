2aa81941e6b3f0cf430ce34da4ff1d78ebf91711
acorreac <aline.cristianecorrea@hotmail.com>
2022-10-16 15:55:12 -0300

    fix: Correção dos forms
    - [x] Correção dos forms no arquivo html comprovante_saque
    - [x] Correção dos forms no arquivo html deposito_confirmacao
    - [x] Correção dos forms no arquivo html saque_confirmacao
    - [x] Atualização de comandos o arquivo build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+rm backend/templates/*
+
 cp frontend/*.html backend/templates
 
+rm backend/static/styles/*
+
 cp frontend/css/*.css backend/static/styles
+
+rm backend/static/img/*
 
 cp frontend/imagens/* backend/static/img
