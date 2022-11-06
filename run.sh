e595a2344671d5d2a8eea52cbf3114e502d92b18
acorreac <aline.cristianecorrea@hotmail.com>
2022-11-06 12:47:07 -0300

    fix: Exclusão de arquivo run

--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-pushd ~/dev/api
-sh build.sh
-popd
-pushd ~/dev/api/backend 
-gnome-terminal -e "flask --app app run --port 5000"
-gnome-terminal -e "flask --app manager_app run --port 5001"
-popd
