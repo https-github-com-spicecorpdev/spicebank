6491cedc7c72620b8c93d8664e3e192181e72dab
Vitor <86271800+Vitaog@users.noreply.github.com>
2022-11-24 12:15:03 -0300

    Ajuste na porta do banco de dados.

--- backend/db.py
+++ backend/db.py
@@ -7,7 +7,7 @@ def connect():
             user="root",
             password="123456",
             host="127.0.0.1",
-            port=3308,
+            port=3306,
             database = "spicebank"
         )
         connection.auto_reconnect = True
